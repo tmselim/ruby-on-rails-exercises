3711fb31c296c96d43f2eecdf561a0c6fca0af5d
Joe Slag <joe@slagwerks.com>
2013-10-23 11:44:56 -0500

    Added unfinished order count to homepage

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,5 +1,6 @@
 class HomepageController < ApplicationController
   def show
     @unpaid_order_count = Order.unpaid.count
+    @unfinished_order_count = Order.unfinished.count
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -4,6 +4,14 @@ class Order < ActiveRecord::Base
 
   scope :unpaid, -> { where("paid_for_on is null") }
 
+  #
+  # Last time around, we implemented 'unpaid' as a scope. It could also
+  # have been a class method, like this:
+  #
+  def Order.unfinished
+    where("completed_on is null")
+  end
+
   private
 
   def completion_date_must_be_in_past
