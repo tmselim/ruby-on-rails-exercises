881a431f9cf6140ea15704f84787aac83ade2095
Joe Slag <joe@slagwerks.com>
2013-10-23 11:35:49 -0500

    Don't allow completion dates in the future

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,13 @@
 class Order < ActiveRecord::Base
   validates :customer_name, :customer_email, :description, :price, presence: true
+  validate :completion_date_must_be_in_past
+
   scope :unpaid, -> { where("paid_for_on is null") }
+
+  private
+
+  def completion_date_must_be_in_past
+    errors.add(:completed_on, "Can't be in the future") if completed_on && completed_on > Time.now
+  end
+
 end
