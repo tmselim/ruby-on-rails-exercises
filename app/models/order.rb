1765fcef230bee971713988cdce027b1c94a2cf1
Paul Cantrell <cantrell@pobox.com>
2013-10-29 23:25:50 -0500

    Show only brand orders ready for fulfillment

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,17 +2,13 @@ class Order < ActiveRecord::Base
   validates :customer_name, :customer_email, :description, :price, :frame_id, presence: true
   validate :completion_date_must_be_in_past
 
+  scope :paid,   -> { where("paid_for_on is not null") }
   scope :unpaid, -> { where("paid_for_on is null") }
   
+  scope :finished,   -> { where("completed_on is not null") }
+  scope :unfinished, -> { where("completed_on is null") }
+  
   belongs_to :frame
-
-  #
-  # Last time around, we implemented 'unpaid' as a scope. It could also
-  # have been a class method, like this:
-  #
-  def Order.unfinished
-    where("completed_on is null")
-  end
 
   private
 
