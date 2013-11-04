60d676a4285c5cd41ed4914ae2a93434ed215760
Paul Cantrell <cantrell@pobox.com>
2013-11-04 17:25:27 -0600

    Added example test for order model

--- test/models/brand_test.rb
+++ test/models/brand_test.rb
@@ -1,7 +1,4 @@
 require 'test_helper'
 
 class BrandTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
 end
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class OrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'order can save' do
+    order = FactoryGirl.build(:order)
+    assert order.save
+  end
+
+  test 'order requires customer name' do
+    order = FactoryGirl.build(:order, customer_name: nil)
+    assert !order.save, "Saved the order without customer_name"
+  end
 end
