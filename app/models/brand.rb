1fc0b5060dae769a7b2e1a3ba92518e7f5962595
Tamer Selim <tamer.selim@barometerit.com>
2013-11-04 19:18:36 -0600

    Exercise 6 - brand name required

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,4 +1,6 @@
 class Brand < ActiveRecord::Base
+  validates :name, presence: true
+  
   has_many :frames
   has_many :orders, through: :frames
 end
--- test/models/brand_test.rb
+++ test/models/brand_test.rb
@@ -1,4 +1,13 @@
 require 'test_helper'
 
 class BrandTest < ActiveSupport::TestCase
+  test 'brand can save' do
+    brand = FactoryGirl.build(:brand)
+    assert brand.save
+  end
+
+  test 'brand requires name' do
+    brand = FactoryGirl.build(:brand, name: nil)
+    assert !brand.save, "Saved the brand without name"
+  end
 end
